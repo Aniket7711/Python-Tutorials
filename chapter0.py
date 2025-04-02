93e5b5f79ec955ed01fef334d3b6ee5a74a7540c
Aniket Jaiswal <dongon40413@gmail.com>
2025-04-02 21:12:33 +0530

    learning python

---
+++ chapter0.py
@@ -0,0 +1,5 @@
+# print("printing jokes...")
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke)
+"""this is a multi line comment"""
---
+++ python 1 practice/practice1.py
@@ -0,0 +1,12 @@
+print('''Hm, jaana, tu aata nahin,
+Sapnon se jaata nahin,
+Mil jaaye, kya hi baat thi
+Kaamil ho jaata wahin
+Jaana, mere sawaalon ka manzar tu
+Haan, main sookha sa, saara samandar tu
+Haan, gulabi si surkhi jo dikhti thi
+Phir se dikh jaye to ji bhar ke saah bhar loon
+Kaati kitni thi raatein, nahin soya main
+Tujhko kitna bulaaya, phir roya main
+Teri saari wo baatein kyun sone nahin deti
+Sataaye mujhe, haan, phir khoya main''')
