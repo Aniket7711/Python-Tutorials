18d10efc2bf48f3ef87160c0b4ffa2bad7ee8120
Aniket Jaiswal <dongon40413@gmail.com>
2025-04-02 22:58:49 +0530

    Fix typo in square root calculation and add alternative method in prob6.py

--- python 1 practice/practice2/prob6.py
+++ python 1 practice/practice2/prob6.py
@@ -1,4 +1,7 @@
 A = int(input("enter the value of A: "))
 
 print("the squar root of the is :", A**2)
+print("the squar root of the is :", A*A)
+# print("the squar root of the is :", A^2) # not valid in python         
+
 
