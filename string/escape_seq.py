8622047073dabb6e206f5ea35d799b1b30511516
Aniket Jaiswal <dongon40413@gmail.com>
2025-04-03 20:41:45 +0530

    Enhance escape_seq.py with additional escape sequence examples

--- string/escape_seq.py
+++ string/escape_seq.py
@@ -1,5 +1,8 @@
-a = "aniket is a good boy \n but not a bad boy"
+a = "aniket is a good boy \n but not a bad \t boy"
 # \n is used to create a new line in the string
+# \t is used to create a tab space in the string
 print(a)
 
-
+b = "aniket is a\r good boy \n but not a bad  \"boy\""
+# \" is used to print double quotes in the string
+print(b)
