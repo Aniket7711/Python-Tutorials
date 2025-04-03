2d77804c375a6e17794197182e052b4512e60a82
Aniket Jaiswal <dongon40413@gmail.com>
2025-04-03 20:06:55 +0530

    Add examples of string length, slicing, and string methods in practice files

--- string/negative_slicing.py
+++ string/negative_slicing.py
@@ -5,6 +5,12 @@ print(name[0:3])
 print(name[-4:-1])
 print(name[1:4])
 
-print(name[:4])
+print(name[:4]) #is same as name[0:4]
 
-print(name[1:])
+print(name[1:]) # is same as name[1:len(name)]
+
+print(len(name)) # len is used to find the length of the string
+
+#jump sclicing
+word = 'amazing'
+print(word[0:6:2]) # prints the characters from index 0 to 7 with a step of 2
---
+++ string/str_function.py
@@ -0,0 +1,12 @@
+name = 'aniket jaiswal'
+
+print(len(name)) # length of string
+
+print(name.endswith('ket')) # check if string ends with 'ket'
+
+print(name.startswith("ni")) # check if string starts with 'ni'
+print(name.capitalize()) # capitalize first letter of string  
+
+print(name.replace('a','A')) # replace 'a' with 'A' in string
+
+print(name.find('jaiswal')) # find index of 'i' in string
