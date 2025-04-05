ab129c7b0a6713262e42ffdf7c607dcc718a682b
Aniket Jaiswal <dongon40413@gmail.com>
2025-04-05 21:21:15 +0530

    add tuple and list examples with sum and count operations

---
+++ list and tuple/practice 4/q3.py
@@ -0,0 +1,5 @@
+tuple = (1,2,3,45,67,89,0)
+print("The tuple is:", tuple)
+tuple[0] = 100
+# TypeError: 'tuple' object does not support item assignment
+print(tuple[0])
---
+++ list and tuple/practice 4/q4.py
@@ -0,0 +1,3 @@
+list = (23,45,36,27)
+list = sum(list)
+print("The sum of the list is:", list)
