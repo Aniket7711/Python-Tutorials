97bca7808386761b6696b91a198bb15c62074570
Aniket Jaiswal <dongon40413@gmail.com>
2025-04-06 21:58:11 +0530

    add implementations for dictionary and set operations with user input

---
+++ dictionary and set/practice 5/q1.py
@@ -0,0 +1,9 @@
+dicts = {
+    "chaval": "rice",
+    "roti": "bread",
+    "dal": "lentils",
+    "sabzi": "vegetable",   
+}
+dict = input("enter the word u want to search: ")
+
+print(dicts[dict])
