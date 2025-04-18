1a697df5d10287e5342c757cdbcb3bb4c02088ca
Aniket Jaiswal <dongon40413@gmail.com>
2025-04-18 20:50:34 +0530

    add age validation logic with if-elif-else structure in conditional scripts

---
+++ conditional/conditional.py
@@ -0,0 +1,12 @@
+a = int(input("enter your age:"))
+
+#if else ladder
+
+if(a>18):
+    print("you are above the age of consent")
+    print("good for you")
+
+
+else:
+    print("you are below the age of consent")
+    
